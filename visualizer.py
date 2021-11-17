7cc31873bd92ae1985f057de1074fbbfbee7362d
sidharthkumar10500 <sidharthpunia10500@gmail.com>
2021-11-16 23:56:10 -0600

    changed to generate test results for the perceptual loss

--- visualizer.py
+++ visualizer.py
@@ -15,7 +15,7 @@ vgg16 = models.vgg16()
 
 
 # os.chdir('/home/sidharth/sid_notebooks/UNET_GAN2_training/train_results/model_GAN_input_data_mdme_data_loss_type_L1_mode_Full_img/val_split_0.2_learning_rate_0.0001_epochs_10_lambda_1_gen_epoch_10_disc_epoch_20')
-os.chdir('/home/sidharth/sid_notebooks/UNET_GAN2_training/train_results/model_UNET_input_data_mdme_data_loss_type_L1_mode_Full_img/learning_rate_0.0001_epochs_150_lambda_1')
+os.chdir('/home/sidharth/sid_notebooks/UNET_GAN2_training/train_results/model_UNET_input_data_mdme_data_loss_type_Perc_L_mode_Full_img/learning_rate_0.0001_epochs_150_lambda_1_loss_type')
 saved_results = torch.load('saved_weights.pt',map_location='cpu')
 hparams   =  saved_results['hparams']
 hparams.device = 'cpu' #all gpus are clogged
